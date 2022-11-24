d27da8947eb04b789a798ba2a7b109e4af8d71f5
yash-pathak-1997 <yashhpathakk1997@gmail.com>
2022-11-24 16:39:16 +0530

    Attached commands and functions

--- git.py
+++ git.py
@@ -1,9 +1,11 @@
 import os
 import sys
+
+
 class VCS:
     def __init__(self):
-        self.RepoPath=os.getcwd() #initialize with the current working directory
-        self.git=os.path.join(self.RepoPath,".git-vcs")
+        self.RepoPath = os.getcwd()  # initialize with the current working directory
+        self.git = os.path.join(self.RepoPath, ".git-vcs")
 
     def initialize(self):
         if not os.path.exists(self.git):
@@ -11,19 +13,27 @@ class VCS:
         else:
             print("Git Repository has already been initialized!!")
             sys.exit(0)
+
     def add(self):
         pass
+
     def pull(self):
         pass
+
     def push(self):
         pass
+
     def rollback(self):
         pass
+
     def status(self):
         pass
+
     def diff(self):
         pass
+
     def commit(self):
         pass
+
     def log(self):
         pass
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import streamlit as st
 from Logs import log_obj
+from git import VCS
+
 
 if __name__ == "__main__":
+    vcs_obj = VCS()
     st.write("""
         # Monkey Git
         *Monkey see monkey do!*
@@ -9,5 +12,33 @@ if __name__ == "__main__":
     cmd = st.text_input('Git command : ')
 
     if st.button("Execute command"):
-        st.write(cmd)
+        st.write("Current Command : " + cmd)
         log_obj.log(cmd + " command", True)
+        arg_list = cmd.split(" ")
+
+        if arg_list[1] == "init":
+            vcs_obj.initialize()
+
+        elif arg_list[1] == "add":
+            vcs_obj.add()
+
+        elif arg_list[1] == "status":
+            vcs_obj.status()
+
+        elif arg_list[1] == "commit":
+            vcs_obj.commit()
+
+        elif arg_list[1] == "rollback":
+            vcs_obj.rollback()
+
+        elif arg_list[1] == "diff":
+            vcs_obj.diff()
+
+        elif arg_list[1] == "log":
+            vcs_obj.log()
+
+        elif arg_list[1] == "push":
+            vcs_obj.push()
+
+        elif arg_list[1] == "pull":
+            vcs_obj.pull()
